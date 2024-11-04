2ca58b46f810ce1267968a2c317c89150f6e423b
tintin1401 <dana.cardenascr@gmail.com>
2024-11-03 23:04:30 -0600

    please work

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,14 +1,14 @@
 import "../../index.css";
 import { FooterLink } from "./FooterLink";
 import { useTranslation } from "react-i18next";
-
+import logo from "../../assets/imgs/logoCeleste.png";
 export function Footer(){
     const { t } = useTranslation();
     return(
         <footer className="bg-black w-full grid lg:p-12 p-8 pt-5">
             <div className="grid gap-10 w-full grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] text-white">
                 <section className="max-w-[250px] text-justify">
-                    <img className="w-1/2" src="../src/assets/imgs/logo-celeste.png" alt="Logo" />
+                    <img className="w-1/2" src={logo} alt="Logo" />
                     <p className="text-white text-base mt-8">{t('footerEslogan')}</p>
                 </section>
 
--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -6,6 +6,7 @@ import { useUser } from "../../context/UserContext.jsx";
 import { useDarkModeContext,useSidebarContext } from "../../context/AppContext.jsx";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
 import { LanguageDropdown } from "../dropdown/LanguageDropdown.jsx";
 
 
@@ -45,20 +46,20 @@ export function Sidebar() {
                 />
             </div>
             {user && user.user_type_id === 1 && (
-                <a href="/create-post" className="relative group mt-[10px] mb-[10px] w-full h-[45px] flex justify-center items-center rounded-lg bg-[#FFD8E4] hover:bg-[#f8c3d4] dark:hover:bg-[##404040] transition-colors duration-300 cursor-pointer" >
+                <NavLink to="/create-post" className="relative group mt-[10px] mb-[10px] w-full h-[45px] flex justify-center items-center rounded-lg bg-[#FFD8E4] hover:bg-[#f8c3d4] dark:hover:bg-[##404040] transition-colors duration-300 cursor-pointer" >
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" /></svg>
                     <span className={`ml-2 flex transition-all duration-500 whitespace-nowrap ${sidebarWidth === "80px" ? 'hidden' : 'inline'}`}>{t('createNew')}</span>
                     <span className="absolute left-full top-1/2 transform -translate-y-1/2 ml-2 p-3 bg-white dark:bg-[#2a2a2a] dark:text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                         {t('createNew')}
                     </span>
-                </a>
+                </NavLink>
             )}
             
             <nav>
                 <ul>
                     <li className="flex mb-[10px] mt-[10px]">
-                        <a onClick={() => selectItem(0)} href="/home" className={`relative group w-full h-[45px] flex items-center rounded-lg transition-colors duration-300 cursor-pointer ${selecItem === 0 ? 'bg-[#E8DEF8] dark:bg-[#484848]' : 'hover:bg-[#E8DEF8] dark:hover:bg-[#484848]'}`}>
+                        <NavLink onClick={() => selectItem(0)} to="/home" className={`relative group w-full h-[45px] flex items-center rounded-lg transition-colors duration-300 cursor-pointer ${selecItem === 0 ? 'bg-[#E8DEF8] dark:bg-[#484848]' : 'hover:bg-[#E8DEF8] dark:hover:bg-[#484848]'}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 min-w-[50px] dark:stroke-white">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
                         </svg>
@@ -66,10 +67,10 @@ export function Sidebar() {
                         <span className="absolute left-full top-1/2 transform -translate-y-1/2 ml-2 p-3 bg-white dark:bg-[#2a2a2a] dark:text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                             {t('sHome')}
                         </span>
-                        </a>
+                        </NavLink>
                     </li>
                     <li className="flex mb-[10px] mt-[10px]">
-                        <a onClick={() => selectItem(1)} href={profileUrl} className={`relative group w-full h-[45px] flex items-center rounded-lg transition-colors duration-300 cursor-pointer ${selecItem === 1 ? 'bg-[#E8DEF8] dark:bg-[#484848]' : 'hover:bg-[#E8DEF8] dark:hover:bg-[#484848]'}`}>
+                        <NavLink onClick={() => selectItem(1)} to={profileUrl} className={`relative group w-full h-[45px] flex items-center rounded-lg transition-colors duration-300 cursor-pointer ${selecItem === 1 ? 'bg-[#E8DEF8] dark:bg-[#484848]' : 'hover:bg-[#E8DEF8] dark:hover:bg-[#484848]'}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 min-w-[50px] dark:stroke-white">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                         </svg>
@@ -77,10 +78,10 @@ export function Sidebar() {
                         <span className="absolute left-full top-1/2 transform -translate-y-1/2 ml-2 p-3 bg-white dark:bg-[#2a2a2a] dark:text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                             {t('profile')}
                         </span>
-                        </a>
+                        </NavLink>
                     </li>
                     <li className="flex mb-[10px] mt-[10px]">
-                        <a onClick={() => selectItem(2)} href="/mapWithRoute" className={`relative group w-full h-[45px] flex items-center rounded-lg transition-colors duration-300 cursor-pointer ${selecItem === 2 ? 'bg-[#E8DEF8] dark:bg-[#484848]' : 'hover:bg-[#E8DEF8] dark:hover:bg-[#484848]'}`}>
+                        <NavLink onClick={() => selectItem(2)} to="/mapWithRoute" className={`relative group w-full h-[45px] flex items-center rounded-lg transition-colors duration-300 cursor-pointer ${selecItem === 2 ? 'bg-[#E8DEF8] dark:bg-[#484848]' : 'hover:bg-[#E8DEF8] dark:hover:bg-[#484848]'}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 min-w-[50px] dark:stroke-white">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z" />
                         </svg>
@@ -88,10 +89,10 @@ export function Sidebar() {
                         <span className="absolute left-full top-1/2 transform -translate-y-1/2 ml-2 p-3 bg-white dark:bg-[#2a2a2a] dark:text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                             {t('Places')}
                         </span>
-                        </a>
+                        </NavLink>
                     </li>
                     <li className="flex mb-[10px] mt-[10px]">
-                        <a onClick={() => selectItem(3)} href="/favorites" className={`relative group w-full h-[45px] flex items-center rounded-lg transition-colors duration-300 cursor-pointer ${selecItem === 3 ? 'bg-[#E8DEF8] dark:bg-[#484848]' : 'hover:bg-[#E8DEF8] dark:hover:bg-[#484848]'}`}>
+                        <NavLink onClick={() => selectItem(3)} to="/favorites" className={`relative group w-full h-[45px] flex items-center rounded-lg transition-colors duration-300 cursor-pointer ${selecItem === 3 ? 'bg-[#E8DEF8] dark:bg-[#484848]' : 'hover:bg-[#E8DEF8] dark:hover:bg-[#484848]'}`}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 min-w-[50px] dark:stroke-white">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                         </svg>
@@ -99,7 +100,7 @@ export function Sidebar() {
                         <span className="absolute left-full top-1/2 transform -translate-y-1/2 ml-2 p-3 bg-white dark:bg-[#2a2a2a] dark:text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                             {t('favorites')}
                         </span>
-                        </a>
+                        </NavLink>
                     </li>
                     
                 </ul>
