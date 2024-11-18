24687aaf763a5808dcf64313deea0f0d0a3ae1a6
tintin1401 <dana.cardenascr@gmail.com>
2024-11-18 09:31:55 -0600

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { SignUpCompanies } from "./components/auth/SignUpCompanies";
+import { SignUpCompanies } from "./components/auth/SignUpCompanies.jsx";
 import { Routes, Route, Navigate } from "react-router-dom";
-import { SignUpUsers } from "./components/auth/SignUpUsers";
-import { Landing } from "./components/routes/Landing";
-import { SignIn } from "./components/auth/SignIn";
-import { SignInCompanies } from "./components/auth/SignInCompanies";
+import { SignUpUsers } from "./components/auth/SignUpUsers.jsx";
+import { Landing } from "./components/routes/Landing.jsx";
+import { SignIn } from "./components/auth/SignIn.jsx";
+import { SignInCompanies } from "./components/auth/SignInCompanies.jsx";
 import logo from "./assets/imgs/Place1.jpg";
-import { CardLocation } from "./components/cards/CardLocation";
-import { Home } from "./components/routes/Home";
-import { AccountSettings } from "./components/profile/AccountSettings"
-import { AccountSettingsCompany } from "./components/profile/AccountSettingsCompany"
-import { MapPage } from './components/map/MapPage';
-import {PersonalInformation} from "./components/profile/PersonalInformation"
-import {PersonalInformationCompany} from "./components/profile/PersonalInformationCompany"
+import { CardLocation } from "./components/cards/CardLocation.jsx";
+import { Home } from "./components/routes/Home.jsx";
+import { AccountSettings } from "./components/profile/AccountSettings.jsx"
+import { AccountSettingsCompany } from "./components/profile/AccountSettingsCompany.jsx"
+import { MapPage } from './components/map/MapPage.jsx';
+import {PersonalInformation} from "./components/profile/PersonalInformation.jsx"
+import {PersonalInformationCompany} from "./components/profile/PersonalInformationCompany.jsx"
 import { InformationCompany } from "./components/profile/InformationCompany.jsx";
-import { SearchResults } from "./components/routes/SearchResults";
+import { SearchResults } from "./components/routes/SearchResults.jsx";
 import { ForgotPassword } from './components/password/ForgotPassword.jsx';
 import { ForgotPasswordCompany } from './components/password/ForgotPasswordCompany.jsx';
 import { ComponentProvider } from './components/hooks/useComponentContext.jsx';
@@ -21,10 +21,12 @@ import { CreatePost } from "./components/routes/CreatePost.jsx";
 import { Categories } from "./components/routes/Categories.jsx";
 import { Favorites } from "./components/routes/Favorites.jsx";
 import { UpdatePost } from "./components/routes/UpdatePost.jsx";
-import { RouteMap } from "./components/map/RouteMap";
+import { RouteMap } from "./components/map/RouteMap.jsx";
 import { AppProvider  } from "./context/AppContext.jsx";
-
-
+import { CompanyDetails } from "./components/cards/CompanyDetails.jsx";
+import { About } from "./components/ui/landing/About.jsx";
+import { Contact } from "./components/ui/landing/Contact.jsx";
+import { TermsAndConditions } from "./components/ui/landing/TermsAndConditions.jsx";
 export function App() {
 
  
@@ -53,7 +55,10 @@ export function App() {
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/forgot-password-company" element={<ForgotPasswordCompany />} />
                 <Route path="/favorites" element={<Favorites  />} />
-
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/company-details" element={<CompanyDetails />} />
+                <Route path="/terms" element={<TermsAndConditions />} />
                 <Route path="/create-post" element={<CreatePost />} />
                 <Route path="/update-post/:id" element={<UpdatePost />} />
                 <Route path="/*" element={<Navigate to="/" />} />
